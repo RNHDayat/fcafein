3aa749a6b95c6f3a562d044b6dccec4dd3e90d3d
NightLabs <90570798+RNHDayat@users.noreply.github.com>
2023-09-16 06:00:38 +0700

    Revert "test"

--- lib/model/database.dart
+++ lib/model/database.dart
@@ -335,20 +335,6 @@ class Followers {
   }
 }
 
-class ChangeName {
-  final int id;
-  final String fullname;
-
-  const ChangeName({required this.id, required this.fullname});
-
-  factory ChangeName.fromJson(Map<String, dynamic> json) {
-    return ChangeName(
-      id: json['id'],
-      fullname: json['fullname'],
-    );
-  }
-}
-
 class Following {
   String? following_id, follow_status;
 
